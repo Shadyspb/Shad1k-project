0803f8eab154c0389c256245d3406e3f47842c48
shady <Shad1k@myttk.ru>
2017-08-09 23:42:08 +0300

    Lesson 1 the fix

---
+++ Lesson_1_fix/Quadratic.rb
@@ -0,0 +1,21 @@
+puts "Введите 1 коэффициент"
+a = gets.chomp.to_f
+
+puts "Введите 2 коэффициент"
+b = gets.chomp.to_f
+
+puts "Введите 3 коэффициент"
+c = gets.chomp.to_f
+
+d = b ** 2 - (4 * a * c)
+
+if d > 0
+  x1 = (-b + Math.sqrt(d)) / (2 * a)
+  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  puts "Дискриминант = #{d}, 1 корень = #{x1}, 2 корень = #{x2}"
+elsif d == 0
+  x1 = -b / (2 * a)
+  puts "Дискриминант = #{d}, 1 корень = #{x1}"
+else
+  puts "Корней нет"
+end
