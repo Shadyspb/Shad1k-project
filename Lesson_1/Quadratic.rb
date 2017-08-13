cd4beafc3d6716382bef3d3372027e22efaf94ca
shady <Shad1k@myttk.ru>
2017-08-13 16:21:43 +0300

    Lesson 1 the 5 fix

--- Lesson_1/Quadratic.rb
+++ Lesson_1/Quadratic.rb
@@ -1,4 +1,3 @@
-require 'mathn'
 puts "Введите 1 коэффициент"
 a = gets.chomp.to_f
 
@@ -10,15 +9,13 @@ c = gets.chomp.to_f
 
 d = b ** 2 - (4 * a * c)
 
-qu = Math.sqrt(d)
-
-if d > 0
+if d < 0
+  puts "Корней нет"
+elsif d == 0
+  puts "Дискриминант = #{d.round}. Корень равен 0"
+else
+  qu = Math.sqrt(d)
   x1 = ((-b + qu) / 2 * a).round(3)
   x2 = ((-b - qu) / 2 * a).round(3)
   puts "Дискриминант = #{d}, 1 корень = #{x1}, 2 корень = #{x2}"
-elsif d == 0
-  x1 = -b / (2 * a)
-  puts "Дискриминант = #{d}, 1 корень = #{x1}"
-elsif d < 0
-  puts "Корней нет"
 end
