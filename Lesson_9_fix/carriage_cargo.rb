f5334b0e0c45add79aec49a4ca233ce9b006c19e
shady <Shad1k@myttk.ru>
2017-11-01 00:14:21 +0300

    Lesson 9 Fix 1

---
+++ Lesson_9_fix/carriage_cargo.rb
@@ -0,0 +1,20 @@
+class CarriageCargo < Carriage
+  def initialize(num, capacity)
+    super(:cargo)
+    @num = num.to_i
+    @capacity = capacity.to_f
+    @load_capacity = 0
+  end
+
+  def load_capacity(capacity)
+    @load_capacity += capacity if @capacity - capacity > 0
+  end
+
+  def occupied_place
+    @capacity - @load_capacity
+  end
+
+  def to_s
+    "Номер вагона - #{@num} тип - #{type}, занятый объём - #{@load_capacity}, cвободный объем - #{occupied_place}"
+  end
+end
